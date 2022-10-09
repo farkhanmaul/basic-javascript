7b3b10aa1c3874c334799e03e64d24db27687d8f
Farkhan Maulana <farkhanmaul@gmail.com>
2022-10-09 11:59:12 +0700

    Delete coba.js

--- coba.js
+++
@@ -1,8 +0,0 @@
-const newObject = {
-  satu: "satu",
-  dua: "dua",
-  tiga: true,
-};
-localStorage.setItem("newItem", JSON.stringify(newObject));
-const getObject = localStorage.getItem("newItem");
-console.log(`${getObject.satu} ${getObject.dua} ${getObject.tiga}`);
